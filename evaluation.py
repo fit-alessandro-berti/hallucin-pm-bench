3e65fb9916e9a5f394da1619b7102720e07d2981
Alessandro Berti <alessandro.berti@fit.fraunhofer.de>
2025-09-22 09:41:24 +0200

    intermediate commit

--- evaluation.py
+++ evaluation.py
@@ -2,7 +2,7 @@ import os, time
 from common import *
 
 
-EVALUATING_MODEL_NAME = "openai/gpt-4.1"
+EVALUATING_MODEL_NAME = "x-ai/grok-4-fast:free"
 
 
 def evaluate(model_name=EVALUATING_MODEL_NAME, target_directory="evaluations", include_ground_truth_answer=True, filter_self=False, parameters=None):
